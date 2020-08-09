48abf6b946e95a7e3a12477c96307cc7eaf6b195
yankoBede <yanko.nikolov@bedegaming.com>
2020-08-09 20:19:36 +0300

    added page not found image

--- book-universe/src/pages/error/index.js
+++ book-universe/src/pages/error/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import PageLayout from '../../components/page-layout';
+import styles from './index.module.css'
 
 const ErrorPage = () => {
   return (
     <PageLayout>
-      <div>Error Page</div>
+      <div>
+        <img class={styles["not-found-image"]} src="http://delo.gis-sofia.bg/uploads/assets/404.png" alt="404"/>
+    </div>
     </PageLayout>
   )
 }
